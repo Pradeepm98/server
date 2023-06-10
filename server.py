45dcb056e611b1fa3bed8921c56237238625ea28
Pradeepm98 <132885368+Pradeepm98@users.noreply.github.com>
2023-06-10 16:09:13 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,7 +9,21 @@ count = 0
 def hello_name(name):
     global count
     count += 1
+
+    # Log when count reaches 5
+    if count == 5:
+        logging.info('Count reached 5')
+
+    # Log when count reaches 10
+    if count == 10:
+        logging.info('Count reached 10')
+
+    # Log when count reaches 25
+    if count == 25:
+        logging.info('Count reached 25')
+
     return f'Hello, {name}! Count: {count}'
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     app.run(host='0.0.0.0', port=80)
