e1c93d58f143b55a966fe5f12d7915c8eb2217bb
Pradeepm98 <132885368+Pradeepm98@users.noreply.github.com>
2023-06-10 15:17:23 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+count = 0
+
 @app.route('/hello/<name>')
 def hello_name(name):
-    return f'Hello, {name}!'
+    global count
+    count += 1
+    return f'Hello, {name}! Count: {count}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
